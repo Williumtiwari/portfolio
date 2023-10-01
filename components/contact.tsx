9d1606ef4a61de5eba88fb33d6cc4bbc9dc67dae
Willium Tiwari <williumtiwari123@gmail.com>
2023-10-02 00:02:33 +0530

    Modified

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="mailto:williumtiwari123@gmail.com">
+          williumtiwari123@gmail.com
         </a>{" "}
         or through this form.
       </p>
