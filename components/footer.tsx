9a501ddb0fe1bf14e3ccb6965c9a87c42481c959
Willium Tiwari <williumtiwari123@gmail.com>
2025-02-21 23:46:13 +0530

    Updated Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-8 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2023 @Willium Tiwari. All rights reserved.
+        &copy; 2025 @Willium Tiwari. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
